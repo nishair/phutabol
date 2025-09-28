3fe5f61352c119205b959333412cb9b1fa157458
hackerrank <me@hackerrank.com>
2025-09-27 19:37:34 -0700

    Initial commit

---
+++ phutabol/__init__.py
@@ -0,0 +1,9 @@
+"""
+Phutabol - Soccer Match Prediction System
+
+A comprehensive soccer analytics and prediction system that uses various statistical models
+and machine learning techniques to predict match outcomes.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Anish Nair"
